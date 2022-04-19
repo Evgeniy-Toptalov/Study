3810ac51219b1eb68ba2b5d07c6d8728635b6c98
Evgeniy_Toptalov <toptalov92@mail.ru>
2022-04-19 23:24:03 +0300

    DZ Java2Lesson4 all work

--- ClientChat/src/main/java/com/example/clientchat/ClientChat.java
+++ ClientChat/src/main/java/com/example/clientchat/ClientChat.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class ClientChat extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(ClientChat.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(ClientChat.class.getResource("chat-template.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Онлайн чат");
         stage.setScene(scene);
